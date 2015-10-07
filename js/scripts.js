110c15ed79006d1626afeaf46e46731c36a28b2a
sarastillwell <sarastillwell@Saras-MacBook-Air.local>
2015-10-06 21:03:48 -0700

    add to scripts without making variables yet.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(#inputlines).sumbit(function() {
+    $("destination").append("blah blah");
+    $("restaurant").append("blah blah");
+    $("holiday").append("blah blah");
+    $("epicodus").append("blah blah");
+
+    $("#list").show();
+  });
+});
